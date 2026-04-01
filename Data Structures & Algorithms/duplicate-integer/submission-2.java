ac7d172816b4c53b61a462638403839340423777
Daniel <daniel.lukins1@gmail.com>
2026-04-01 11:52:37 +1100

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++){
+            for (int j = 0; j < nums.length; j++){
+                if (nums[j] == nums[i] && i != j){
+                    return true;
+                }
+                
+            }
+        }
+        return false;
+        
+        
+    }
+}
