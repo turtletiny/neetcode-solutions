8f34ef50f1aa6970d62bbc7a8d5bbeafcdb893bc
Daniel <daniel.lukins1@gmail.com>
2026-04-01 11:59:09 +1100

    Clarify inner loop logic in hasDuplicate method
    Updated inner loop comment for clarity.

--- Data Structures & Algorithms/duplicate-integer/submission-1.java
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean hasDuplicate(int[] nums) {
         for (int i = 0; i < nums.length; i++){
-            for (int j = i+1; j < nums.length; j++){
+            for (int j = i+1; j < nums.length; j++){   //j is always i+1; this is so the check is for every number after i **NOTE: j is updated as i increases**
                 if (nums[j] == nums[i]){
                     return true;
                 }
@@ -13,3 +13,5 @@ class Solution {
         
     }
 }
+
+//brute force solution - checking every number against every other number
