278468d0a558c1eec3c544aabcb0bbda1e38629f
Daniel <daniel.lukins1@gmail.com>
2026-06-05 10:06:01 +1000

    Add: sort-colors - submission-7

---
+++ Data Structures & Algorithms/sort-colors/submission-7.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        int i = 0;
+        while (i <= r) {
+            if (nums[i] == 0) {
+                nums[i] = nums[l];
+                nums[l] = 0;
+                l++;
+            } else if (nums[i] == 2) {
+                nums[i] = nums[r];
+                nums[r] = 2;
+                r--;
+                i--;
+            }
+
+            i++;
+        }
+    }
+}
