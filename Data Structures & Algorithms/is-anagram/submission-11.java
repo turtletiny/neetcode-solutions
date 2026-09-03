da4ae6645b6639112231865213cee52f4c605ed7
Daniel <daniel.lukins1@gmail.com>
2026-09-03 11:06:43 +1000

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 97]++;
+            count[t.charAt(i) - 97]--;
+        }
+        
+        for (int n : count) {
+            if (n != 0) return false;
+        }
+        return true;
+    }
+}
